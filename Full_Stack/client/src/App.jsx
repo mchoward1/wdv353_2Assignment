aaa74f10124cf10c98896bf5eb0d2df4ab23f465
Michael Howard <mchoward1@student.fullsail.edu>
2024-03-31 21:13:14 -0500

    Las Changes

--- Full_Stack/client/src/App.jsx
+++ Full_Stack/client/src/App.jsx
@@ -1,24 +1,19 @@
 import "react";
 import "./App.css";
-import SearchBar from "./components/SearchBar";
 import Results from "./components/Results";
 import API from './API/index';
 
 function App() {
-  const handleSearch = async event => {
-    event.preventDefault();
-    console.log("Search button clicked!", event.target.search.value);
-    const response = await API.fetchMakes();
-    console.log("From our API!", response.data);
-  };
+  // const handleSearch = async event => {
+  //   event.preventDefault();
+  //   console.log("Search button clicked!", event.target.search.value);
+  //   const response = await API.fetchMakes();
+  //   console.log("From our API!", response.data);
+  // };
 
-  return (
-    <>
-      <SearchBar onSubmit={handleSearch} />
-      <Results/>
-    </>
-      
-  )
+    return (
+      <Results />
+    )
 }
 
 export default App;
--- Full_Stack/client/src/components/Results.jsx
+++ Full_Stack/client/src/components/Results.jsx
@@ -1,10 +1,18 @@
 import API from "../API/index";
+import SearchBar from "./SearchBar";
 
 function Results({listing}) {
 
-    const listings = API.fetchMakes();
+    const handleSearch = async event => {
+        event.preventDefault();
+        console.log("Search button clicked!", event.target.search.value);
+        const response = await API.fetchMakes();
+        console.log("From our API!", response.data);
+    };
+
     return (
         <>
+            <SearchBar onSubmit={handleSearch} />
             <h3>Results</h3>
         </>
     )
